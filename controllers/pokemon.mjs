6952b888836b5267f1f34ecaadb20cda0a9cfa09
Ritesh Raj Moond <moondritesh@gmail.com>
2024-04-09 12:19:02 +0530

    added caching!

--- controllers/pokemon.mjs
+++ controllers/pokemon.mjs
@@ -1,5 +1,7 @@
 import Pokedex from "pokedex-promise-v2";
+import nodeCache from "node-cache";
 const P = new Pokedex();
+const cache = new nodeCache();
 
 export const getHome = (req, res, next) => {
   res.status(200).json("This is home page");
@@ -25,9 +27,21 @@ export const getPokemonByName = async (req, res, next) => {
 export const getPokemonColorByName = async (req, res, next) => {
   let { pokemonColor } = req.params;
   pokemonColor = pokemonColor.toLowerCase().trim();
+
+  const cacheKey = `type:${pokemonColor}`;
+  //If data is present in cache, return it
+  const cachedData = cache.get(cacheKey);
+  if(cachedData) {
+    return res.status(200).json({ pokemon_species: cachedData });
+  }
+
+  //If not present in cache, fetch data from API
   P.getPokemonColorByName(pokemonColor)
     .then((resp) => {
       const { pokemon_species } = resp;
+
+      cache.set(cacheKey, pokemon_species, 3600);
+
       return res.status(200).json({ pokemon_species });
     })
     .catch((err) => {
@@ -41,9 +55,20 @@ export const getPokemonColorByName = async (req, res, next) => {
 export const getPokemonByType = async (req, res, next) => {
   let { pokemonType } = req.params;
   pokemonType = pokemonType.toLowerCase().trim();
+
+  const cacheKey = `type:${pokemonType}`;
+  const cachedData = cache.get(cacheKey);
+
+  if(cachedData) {
+    return res.status(200).json({pokemon: cachedData});
+  }
+
   P.getTypeByName(pokemonType)
     .then((resp) => {
       const { pokemon } = resp;
+
+      cache.set(cacheKey, pokemon, 3600);
+
       return res.status(200).json(pokemon);
     })
     .catch((err) => {
@@ -71,9 +96,19 @@ export const getPokemonByRegion = async (req, res, next) => {
 
   pokemonRegion = regionToGeneration[region];
 
+  const cacheKey = `region:${pokemonRegion}`;
+  const cachedData = cache.get(cacheKey);
+
+  if(cachedData) {
+    return res.status(200).json({pokemon_species: cachedData});
+  }
+
   P.getGenerationByName(pokemonRegion)
     .then((resp) => {
       const { pokemon_species } = resp;
+
+      cache.set(cacheKey, pokemon_species, 3600);
+
       return res.status(200).json({ pokemon_species });
     })
     .catch((err) => {
